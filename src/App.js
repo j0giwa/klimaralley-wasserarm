cd0b9e9fba7f0d72e7afb5f67bba12df0bab19bd
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-04-25 16:20:47 +0200

    Installed Daisy-UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="horizontal">
+    <h1 className="text-3xl center-h text-sky-400 font-bold underline">
+      Hello world!
+    </h1>
+	  </div>
+
   );
 }
 
