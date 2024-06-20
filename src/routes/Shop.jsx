fbf3e6c59a9cda51801f4a1b2105f03b57ccbd46
alexm <alexander.golueke@gmail.com>
2024-06-20 14:50:52 +0200

    import Header from "../components/Header";

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
-import ShopItem from "../components/ShopItem";
-import ShoppingCart from "./Cart";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import ShopItemList from "../components/ShopItemList";
 
 /**
  * Shop page (main part) of the wasserarm-satt game
