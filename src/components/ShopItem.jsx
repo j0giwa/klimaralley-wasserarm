82943658f3ea566323f69fa3fa96ea36263aae78
WalterWendji <rahaman.dongmepi@stud.th-owl.de>
2024-08-21 23:56:56 +0200

    update of frontend

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -41,7 +41,6 @@ function ShopItem({ shopItem, onAdd }) {
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M6 12H18" stroke="#CBC9C9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
-              <span>kg</span>
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 19V5" stroke="#CBC9C9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 <path d="M5 12H19" stroke="#CBC9C9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
