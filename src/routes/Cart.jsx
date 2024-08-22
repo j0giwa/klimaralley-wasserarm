fef8d3d5d76c9a721ec2413e947ccb5fdb37bf70
WalterWendji <rahaman.dongmepi@stud.th-owl.de>
2024-08-22 20:36:44 +0200

    frontend update: the nav menu is ongoing

---
+++ src/routes/Cart.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react"
+import ShoppingCart from "../components/ShoppingCart";
+import Header from "../components/Header";
+
+function Cart() {
+    return (
+        <div>
+            <Header />
+            <ShoppingCart />
+        </div>
+    )
+}
+export default Cart();
