ac6beb850d07a5d338aaf301735fbd6f1eafcd9a
WalterWendji <rahaman.dongmepi@stud.th-owl.de>
2024-08-25 14:28:43 +0200

    image upload

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -17,9 +17,16 @@ function App() {
         <Header />
       </header>
       <main>
-        <div className="container container-main mx-auto mt-56 md:px-5">
-          <span> Kategorien </span>
-          <span>Hello</span>
+        <div className="container container-main mx-auto mt-56 md:px-5 px-3">
+          <span className="text-xl"> Kategorien </span>
+          <ul className="flex justify-between">
+            <li><a href="">Obst</a></li>
+            <li><a href="">Gemüse</a></li>
+            <li><a href="">Nüsse</a></li>
+            <li><a href="">Fleisch & Geflügel</a></li>
+            <li><a href="">Fisch & Meeresfrüchte</a></li>
+            <li><a href="">Milchprodulte</a></li>
+          </ul>
         </div>
         <span>hallo</span>
       </main>
